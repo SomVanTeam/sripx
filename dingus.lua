b4a824e5a586e1d84d5268f04fb4eb05eda3fa1f
Somvan <89217061+SomVanTeam@users.noreply.github.com>
2026-07-03 03:04:47 +0300

    Update dingus.lua

--- dingus.lua
+++ dingus.lua
@@ -1,80 +1,64 @@
 --[[
 loadstring(game:HttpGet((
-""
+"https://raw.githubusercontent.com/SomVanTeam/sripx/refs/heads/main/dingus.lua"
 )))()
 ]]
-local util = {}
-
-function util.safeDestroy(target:Instance)
-    pcall(function()
-        target:Destroy()
-    end)
-end
-
-util.canRemoveESPOnLimit = true
-util.maxESP = 31
-util.ESPHighlights = {}
-function util.simpleESP(target:Instance)
+function simpleESP(target:Instance, fillcolor:Color3)
     print("Highlighting "..target.Name)
     local highlight = Instance.new("Highlight")
     highlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
     highlight.OutlineTransparency = 0
     highlight.OutlineColor = Color3.fromRGB(255, 255, 255)
     highlight.FillTransparency = 0.5
-    highlight.FillColor = Color3.fromRGB(255, 0, 0)
+    highlight.FillColor = fillcolor
     highlight.Enabled = true
-    table.insert(util.ESPHighlights, highlight)
-    if #util.ESPHighlights > util.maxESP then
-        if util.canRemoveESPOnLimit then
-            local oldhighlight = util.ESPHighlights[1]
-            oldhighlight:Destroy()
-        end
-    end
     --highlight.Adornee = target
     highlight.Parent = target
 end
 
-function util.clearESP()
-    for _, esp in pairs(util.ESPHighlights) do
-        if esp then
-            esp:Destroy()
-        end
-    end
-    table.clear(util.ESPHighlights)
-end
+local orion = loadstring(game:HttpGet(("https://raw.githubusercontent.com/jensonhirst/Orion/main/source")))()
 
-local function updateESP()
+local window = orion:MakeWindow({Name = "Prep Test", HidePremium = true, SaveConfig = false, ConfigFolder = "OrionTest"})
+
+local maintab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local function addesp()
     for _, h in pairs(workspace:GetDescendants()) do
         if h and h:IsA("Highlight") then
             h:Destroy()
         end
     end
-    for _, p in pairs(game.Players:GetPlayers()) do
+	for _, p in pairs(game.Players:GetPlayers()) do
         local m = workspace:FindFirstChild(p.Name) or p.Character or nil
         if not m then
-            --for _, cont in pairs(workspace:GetChildren()) do
-            --    if cont.Name == "HighlightContainer" then
-            --        m = cont:FindFirstChild(p.Name)
-            --        if not m then
-                        print(p.Name.." Not Found")
-                        continue
-            --        end
-            --    end
-            --end
+            print(p.Name.." Not Found")
+            continue
         end
-        if m ~= nil then
-            util.simpleESP(m)
-        end
+        simpleESP(m, orion.Flags["hidercolor"].Value)
     end
 end
 
-local roundStartedEvent = game.ReplicatedStorage.Remotes.RoundStarted
+esptab:AddButton({
+	Name = "ESP",
+	Callback = function(Value)
+		addesp()
+	end    
+})
 
-updateESP()
-roundStartedEvent.OnClientEvent:Connect(function(...)
-    print("ROUND STARTED EVENT")
-    task.wait(2)
-    updateESP()
-end)
+esptab:AddColorpicker({
+	Name = "Hunters ESP",
+	Default = Color3.fromRGB(32, 32, 32),
+	Flag = "huntercolor"
+})
 
-print("------------------------------------------------------------------------------")
+esptab:AddColorpicker({
+	Name = "Hiders ESP",
+	Default = Color3.fromRGB(128, 16, 200),
+	Flag = "hidercolor"
+})
+
+orion:Init()
